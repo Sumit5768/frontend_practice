30630013c8e84f677c5028a668a0880dafdf5ab1
Sumit Nandeda <sumitnandeda4@gmail.com>
2026-07-08 22:03:54 +0530

    practice

---
+++ js/array_practice.py
@@ -0,0 +1,13 @@
+let arr = [1 ,2,3,44,568,55,66,77,8];
+let arr1 = arr.map(num => num * 2);
+ console.log(arr1);
+
+ let arr2 = [1 ,2,3,44,568,55,66,77,8];
+ let arr3 = arr.filter(num => num < 50);
+ console.log(arr3);
+
+
+
+ 
+
+
